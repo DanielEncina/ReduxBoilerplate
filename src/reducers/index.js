9832043249cdebaca78c2646147b9c20bf2b6a6c
Daniel Encina <dani.encina@alumnos.duoc.cl>
2018-09-09 02:58:28 -0300

    adding new reducers to index reducers !

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+
 import superHeroesReducer from './superHeroesReducers.js';
+import tabBarReducer from './TabBarReducer.js';
 
 export default combineReducers({
-  superHeroes: superHeroesReducer
+  superHeroes: superHeroesReducer,
+  tabBar: tabBarReducer
 });
