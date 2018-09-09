add6e161a1e76f48d746501e6af0f2b880189f2c
Daniel Encina <dani.encina@alumnos.duoc.cl>
2018-09-09 12:40:25 -0300

    adding data reducer to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import superHeroesReducer from './superHeroesReducers.js';
 import tabBarReducer from './TabBarReducer.js';
+import dataReducer from './dataReducer.js';
 
 export default combineReducers({
   superHeroes: superHeroesReducer,
-  tabBar: tabBarReducer
+  tabBar: tabBarReducer,
+  data: dataReducer,
 });
